3845b81d84072143cadd8e9e1719e846f19339a0
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-07-04 16:21:01 -0400

    add user input functionality

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,5 +1,7 @@
 package com.twu.biblioteca;
 
+import java.util.Scanner;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
@@ -9,13 +11,35 @@ public class BibliotecaApp {
         System.out.println("*** " + startup.greetUserOnStart() + " ***");
         System.out.println("*****************************************************************");
 
-        LibraryDatabase database = new LibraryDatabase();
-        BookPrinter bookPrinter = new BookPrinter();
+        Scanner reader = new Scanner(System.in);
 
         System.out.println();
-        System.out.println("Currently available books:");
-        System.out.println();
-        bookPrinter.printBooksInfoToScreen(database.getCurrentBooks());
+        System.out.println("Please type \"list books\" and press ENTER to view all available books");
+        String userInput = reader.nextLine();
+
+        while (true) {
+
+            if (userInput.equals("list books")) {
+                LibraryDatabase database = new LibraryDatabase();
+                BookPrinter bookPrinter = new BookPrinter();
+
+                System.out.println();
+                System.out.println("Currently available books:");
+                System.out.println();
+                bookPrinter.printBooksInfoToScreen(database.getCurrentBooks());
+                reader.close();
+                break;
+            } else {
+                System.out.println("Select a valid option!");
+                System.out.println("Please type \"list books\" and press ENTER to view all available books");
+                userInput = reader.nextLine();
+            }
+
+
+
+
+        }
+
 
 
     }
